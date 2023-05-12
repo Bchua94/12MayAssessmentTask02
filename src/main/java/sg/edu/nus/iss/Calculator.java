260dde6fd2a39ba80d174a808115fd38e1b86e01
Bryan Chua <Bryanchua.1994@gmail.com>
2023-05-12 16:40:00 +0800

    final commit

---
+++ src/main/java/sg/edu/nus/iss/Calculator.java
@@ -0,0 +1,58 @@
+package sg.edu.nus.iss;
+
+
+
+import java.util.Scanner;
+
+public class Calculator 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Welcome.");
+
+        Scanner scanner = new Scanner(System.in);
+        String option = "";
+
+        while (!option.equals("exit")) {
+            System.out.println("> ");
+            option = scanner.nextLine();
+
+            if (option.equals("exit")) {
+                System.out.println("Bye bye");
+                
+            }
+            else {
+                String[] parts = option.split(" ");
+
+                double number1 = Double.parseDouble(parts[0]);
+
+                String operator = parts[1];
+
+                double number2 = Double.parseDouble(parts[2]);
+
+                double result = 0;
+
+                if (operator.equals("+")) {
+                    result = number1 + number2;
+                    System.out.println(result);
+                }
+
+                else if (operator.equals("-")) {
+                    result = number1 - number2;
+                    System.out.println(result);
+
+                }
+                else if (operator.equals("*")) {
+                    result = number1 * number2;
+                    System.out.println(result);
+                }
+
+                else if (operator.equals("/")) {
+                    result = number1 / number2;
+                    System.out.println(result);
+                }
+        }
+        }
+        scanner.close();
+    }
+}
